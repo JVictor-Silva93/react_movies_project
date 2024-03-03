4ff643a4aaf31d6500678cde11acb208ac3ecc82
JVictor-Silva93 <victor@DESKTOP-0R52UO8>
2024-03-02 19:19:03 -0500

    finalized!

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
 // a424ac3c
 
 const API_URL = "http://www.omdbapi.com?apikey=a424ac3c"
 
 const App = () => {
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
         const data = await response.json();
-        console.log(data.Search);
+        
+        setMovies(data.Search);
     }
 
     useEffect(() => {
@@ -33,29 +37,26 @@ const App = () => {
 
             <div className="search">
                 <input type="text" placeholder="Search for movies" 
-                value='Superman'
-                onChange={() => {}}/>
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}/>
                 <img src={SearchIcon} 
                 alt="Search" 
-                onClick={() => {}}/>
+                onClick={() => searchMovies(searchTerm)}/>
             </div>
 
-            <div className="container">
-                <div className="movie">
-                    <div>
-                        <p>{movie1.Year}</p>
-                    </div>
-
-                    <div>
-                        <img src={movie1.Poster !== "N/A" ? movie1.Poster : "https://via.placeholder.com/400"} alt={movie1.Title} />
-                    </div>
-
-                    <div>
-                        <span>{movie1.Type}</span>
-                        <h3>{movie1.Title}</h3>
-                    </div>
+            {movies?.length > 0
+                ? (
+                <div className="container">
+                    {movies.map((movie, index) => (
+                        <MovieCard key={index} movie={movie} />
+                    ))}
                 </div>
-            </div>
+                ) : (
+                    <div className="Empty">
+                        <h2>No movies found</h2>
+                    </div>
+                )}
+
         </div>
     );
 };
